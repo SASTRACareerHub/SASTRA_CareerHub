b8156c0ac5e92a7b33f54ea76c4b28b837b0de41
Tejeswar-16 <tejeswarsakthidaran@gmail.com>
2025-08-03 15:21:43 +0530

    basic validations were done on login and sign up

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,31 @@ import { useState } from "react";
 export default function Home() {
 
   const [signUpClick,setSignUpClick] = useState(false);
+  const [signInEmail,setSignInEmail] = useState("");
+  const [signInPassword,setSignInPassword] = useState("");
+  const [signUpName,setSignUpName] = useState("");
+  const [signUpEmail,setSignUpEmail] = useState("");
+  const [signUpPassword,setSignUpPassword] = useState("");
+  const [signUpConfirmPassword,setSignUpConfirmPassword] = useState("");
+  const [pwdError,setPwdError] = useState("");
 
   function handleSignUpClick(){
     setSignUpClick(true);
+    setSignInEmail("");
+    setSignInPassword("");
   }
 
   function handleSignInFromSignUp(){
     setSignUpClick(false);
+    setSignUpName("");setSignUpEmail("");
+    setSignUpPassword("");setSignUpConfirmPassword("");
+  }
+
+  function handlePasswordCheck(passwordValue,confirmPasswordValue){
+    if (passwordValue !== confirmPasswordValue)
+      setPwdError("Passwords do not match");
+    else  
+      setPwdError("");
   }
 
   return (
@@ -22,15 +40,15 @@ export default function Home() {
       </div>
       {
         !signUpClick &&
-        <div className="font-sans bg-white rounded-3xl my-10 shadow-2xl m-auto h-80 w-70 md:m-auto md:my-10 md:h-80 md:w-120">
+        <div className="font-sans bg-white rounded-3xl my-10 shadow-2xl shadow-yellow-500 m-auto h-80 w-70 md:m-auto md:my-10 md:h-80 md:w-120">
           <h1 className="select-none flex justify-center font-sans font-semibold text-2xl pt-2">Sign In</h1>
           <h1 className="select-none flex justify-center mx-2 md:mx-0 font-sans text-sm md:text-lg text-gray-600 pb-2 italic">Streamline Your Journey to Success</h1>
           <hr className="text-gray-400"></hr>
           <form>
             <div className="flex flex-col">
-              <input required className="font-sans p-2 rounded-lg m-4 border border-gray-400 h-10" type="email" placeholder="SASTRA Email"/>
-              <input required className="font-sans p-2 rounded-lg mx-4 border border-gray-400 h-10" type="password" placeholder="Password"/>
-              <button type="submit" className="font-sans rounded-lg m-4 text-white text-xl bg-gray-400 h-10 hover:cursor-pointer">Sign In</button>
+              <input onChange={(e) => setSignInEmail(e.target.value)} required className="font-sans p-2 rounded-lg m-4 border border-gray-400 h-10" type="email" placeholder="SASTRA Email"/>
+              <input onChange={(e) => setSignInPassword(e.target.value)} required className="font-sans p-2 rounded-lg mx-4 border border-gray-400 h-10" type="password" placeholder="Password"/>
+              <button type="submit" className={(signInEmail === "" || signInPassword === "") ? "font-sans rounded-lg m-4 text-white text-xl bg-gray-400 h-10 hover:cursor-not-allowed" : "font-sans rounded-lg m-4 text-white text-xl bg-black h-10 hover:cursor-pointer"}>Sign In</button>
             </div>
           </form>
           <div className="flex flex-row justify-center">
@@ -42,17 +60,18 @@ export default function Home() {
 
       {
         signUpClick &&
-        <div className="font-sans bg-white rounded-3xl my-5 shadow-2xl m-auto h-110 w-70 md:m-auto md:my-5 md:h-110 md:w-120">
+        <div className="font-sans bg-white rounded-3xl my-5 shadow-2xl shadow-yellow-500 m-auto h-110 w-70 md:m-auto md:my-5 md:h-110 md:w-120">
           <h1 className="select-none flex justify-center font-sans font-semibold text-2xl pt-2">Sign Up</h1>
           <h1 className="select-none flex justify-center mx-2 md:mx-0 font-sans text-sm md:text-lg text-gray-600 pb-2 italic">Streamline Your Journey to Success</h1>
           <hr className="text-gray-400"></hr>
           <form>
             <div className="flex flex-col">
-              <input required className="font-sans p-2 rounded-lg m-4 border border-gray-400 h-10" type="text" placeholder="Name"/>
-              <input required className="font-sans p-2 rounded-lg mx-4 border border-gray-400 h-10" type="email" placeholder="SASTRA Email"/>
-              <input required className="font-sans p-2 rounded-lg m-4 border border-gray-400 h-10" type="password" placeholder="Password"/>
-              <input required className="font-sans p-2 rounded-lg mx-4 border border-gray-400 h-10" type="password" placeholder="Confirm Password"/>
-              <button type="submit" className="font-sans rounded-lg m-4 text-white text-xl bg-gray-400 h-10 hover:cursor-pointer">Sign Up</button>
+              <input value={signUpName} onChange={(e) => setSignUpName((e.target.value).toUpperCase())} required className="font-sans p-2 rounded-lg m-4 border border-gray-400 h-10" type="text" placeholder="Name"/>
+              <input value={signUpEmail} onChange={(e) => setSignUpEmail(e.target.value)}required className="font-sans p-2 rounded-lg mx-4 border border-gray-400 h-10" type="email" placeholder="SASTRA Email"/>
+              <input value={signUpPassword} onChange={(e) => {setSignUpPassword(e.target.value);handlePasswordCheck(e.target.value,signUpConfirmPassword)}}required className="font-sans p-2 rounded-lg m-4 border border-gray-400 h-10" type="password" placeholder="Password"/>
+              <input value={signUpConfirmPassword} onChange={(e) => {setSignUpConfirmPassword(e.target.value);handlePasswordCheck(signUpPassword,e.target.value)}}required className="font-sans p-2 rounded-lg mx-4 border border-gray-400 h-10" type="password" placeholder="Confirm Password"/>
+              <label className="font-sans mx-4 mt-2 text-sm text-red-500">{pwdError}</label>
+              <button type="submit" className={(signUpName === "" || signUpEmail === "" || signUpPassword === "" || signUpConfirmPassword === "")? "font-sans rounded-lg m-4 text-white text-xl bg-gray-400 h-10 hover:cursor-not-allowed" : "font-sans rounded-lg m-4 text-white text-xl bg-black h-10 hover:cursor-pointer"}>Sign Up</button>
             </div>
           </form>
           <div className="flex flex-row justify-center">
